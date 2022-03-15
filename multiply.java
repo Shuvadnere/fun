90b8bfb52b72a537f2591510a480996dcc7e9aa2
Shubham Vadnere <101618357+Shuvadnere@users.noreply.github.com>
2022-03-15 12:04:35 +0530

    multiply_java_code

---
+++ multiply.java
@@ -0,0 +1,19 @@
+class multiply1{
+
+    static void add (int a, int b, int c)
+
+    {
+
+        int result = a*b*c;
+
+        System.out.println("Multiplication = "+result);
+
+    }
+
+    public static void main(String[] args) {
+
+        add(5,9,8);
+
+    }
+
+}
